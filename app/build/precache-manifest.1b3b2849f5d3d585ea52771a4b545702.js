b2be329000de8824264649030d6f167ce47060b7
chandrakalakakarla <123530194+chandrakalakakarla@users.noreply.github.com>
2023-08-15 18:18:46 +0530

    Add files via upload

---
+++ app/build/precache-manifest.1b3b2849f5d3d585ea52771a4b545702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33e8e00576c9411157d6",
+    "url": "/static/css/main.7a63735b.chunk.css"
+  },
+  {
+    "revision": "33e8e00576c9411157d6",
+    "url": "/static/js/main.cbb76132.chunk.js"
+  },
+  {
+    "revision": "61fe2c24218e1370c5ed",
+    "url": "/static/js/runtime~main.c25113f1.js"
+  },
+  {
+    "revision": "915279245348ce53b136",
+    "url": "/static/js/2.6e22f8b6.chunk.js"
+  },
+  {
+    "revision": "f60355501f08d842a8fe",
+    "url": "/static/js/3.aaf1f5bc.chunk.js"
+  },
+  {
+    "revision": "9aa917d7e9a8874898dcf0ec1fa380ed",
+    "url": "/index.html"
+  }
+];
