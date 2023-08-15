3fafb2bd87214461288ac7c454c4a48a73033d74
chandrakalakakarla <123530194+chandrakalakakarla@users.noreply.github.com>
2023-08-15 15:29:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port =process.env.PORT || 5000
 
 if(process.env.NODE_ENV === 'production')
 {
-     app.use('/' , express.static(path.join(__dirname,'./app/build'))
+     app.use('/' , express.static(path.join(__dirname,'./app/build')))
 
      app.get('*' , (req, res)=>{
          res.sendFile(path.resolve(__dirname, './app/build/index.html'))
